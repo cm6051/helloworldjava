d5512760d2e788b0747434f3a5b16a9aad9158bc
Matt Saunders <matt.saunders@contino.co.uk>
2015-11-25 10:24:10 +0000

    An awesome change that will not break, surely?

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld
 	{
 		System.out.println("Hello, world!");
 		System.out.println("Now it is Wednesday. Yay. Humpday.");
+		System.out.println("I am a great progammer! Nothing can possibly go wrong!
 	}
 }
